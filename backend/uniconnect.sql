b0b2e39cf6cced07be29100ed34189b7310db3c0
NOUR2972 <narnora576@gmail.com>
2026-03-10 03:53:00 +0200

    update file
    Updated the Users table schema to include AUTO_INCREMENT for id, set NOT NULL constraints for name, email, and password, and changed password length to 255. Also modified the role field to use ENUM type.

--- backend/uniconnect.sql
+++ backend/uniconnect.sql
@@ -1,12 +1,12 @@
-
-CREATE DATABASE uniconnect;
+CREATE DATABASE IF NOT EXISTS uniconnect;
 USE uniconnect;
-CREATE TABLE Users(
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(50),   
-    role VARCHAR(50),
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student','admin', 'investor', 'doctor') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
