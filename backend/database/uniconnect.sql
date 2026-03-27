e55be51791b2fc55124228c862a59618e8a5c45f
kareem helal <koko2006party@gmail.com>
2026-03-27 21:46:01 +0200

    Switching to main branch and adding files

---
+++ backend/database/uniconnect.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS uniconnect;
+USE uniconnect;
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student','admin', 'investor', 'doctor') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
