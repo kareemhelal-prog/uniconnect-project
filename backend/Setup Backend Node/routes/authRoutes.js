18700d2f0d0d5dd41aebb076113df2545e20fde4
‪Alaa samir ‬‏ <ahmedalaasamir831@gmail.com>
2026-04-24 23:31:16 +0300

    Replace userController with authController in routes

--- backend/Setup Backend Node/routes/authRoutes.js
+++ backend/Setup Backend Node/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
-router.post("/register", userController.register);
-router.post("/login", userController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 module.exports = router;
